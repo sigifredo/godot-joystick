e366a83e9365a04b5086e451a3f8818e838c4dde
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2024-09-28 14:02:24 -0500

    Se inicia la creación del joystick.

---
+++ tools/knob.gd
@@ -0,0 +1,30 @@
+extends Sprite2D
+
+var pressing:bool = false
+
+@onready var parent = $".."
+@export var maxLength = 50
+@export var deadzone = 5
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	if pressing:
+		if get_global_mouse_position().distance_to(parent.global_position) <= maxLength:
+			global_position = get_global_mouse_position()
+		else:
+			var angle = parent.global_position.angle_to_point(get_global_mouse_position())
+
+			global_position.x = parent.global_position.x + cos(angle) * maxLength
+			global_position.y = parent.global_position.y + sin(angle) * maxLength
+	else:
+		global_position = lerp(global_position, parent.global_position, delta * 10)
+
+func _on_button_down() -> void:
+	pressing = true
+
+
+func _on_button_up() -> void:
+	pressing = false
