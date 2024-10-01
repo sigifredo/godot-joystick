206c2aeab0535ea03409888d556c3fc0a18775cc
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2024-09-30 23:45:11 -0500

    Se agrega animación al mover el joystick.
    Esta animación consiste en agregar una opacidad al sprite del anillo. Cuando se está usando el joystick, el anillo se pone transparente, cuando no se está usando, es totalmente sólido (opacity = 1).

--- tools/joystick.gd
+++ tools/joystick.gd
@@ -2,6 +2,10 @@ extends Node2D
 class_name Joystick
 
 @onready var knob: Knob = $Knob
+@onready var ring: Sprite2D = $Ring
+
+@export_range(0.1, 0.5)
+var ring_opacity: float = 0.25
 
 signal change_position(pos_vector: Vector2)
 
@@ -9,4 +13,10 @@ func _ready() -> void:
 	knob.change_position.connect(_slot_change_position)
 
 func _slot_change_position(pos_vector: Vector2):
+
+	if (abs(pos_vector.x) + abs(pos_vector.y)) > 0.0:
+		ring.modulate.a = lerp(ring.modulate.a, ring_opacity, 0.1)
+	else:
+		ring.modulate.a = lerp(ring.modulate.a, 1.0, 0.1)
+
 	change_position.emit(pos_vector)
